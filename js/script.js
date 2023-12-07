41ae1cd1acd4fc73fc409312ef1e8658231cdb43
nadineRoldao <vitorianadineroldao@gmail.com>
2023-12-07 14:35:55 -0300

    criando telas real estate owner automation

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const select = document.querySelector(".select");
+const options_list = document.querySelector(".options-list");
+const options = document.querySelectorAll(".option");
+const faIcon = document.querySelector('.arrow')
+
+//show & hide options list
+select.addEventListener("click", () => {
+  options_list.classList.toggle("active");
+  faIcon.classList.toggle('active')
+
+});
+
+//select option
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    options.forEach((option) => {option.classList.remove('selected')})
+    select.querySelector("span").innerHTML = option.innerHTML
+    option.classList.add("selected")
+    options_list.classList.toggle("active")
+    faIcon.classList.toggle('active')
+
+  })
+})
+
+const select2 = document.querySelector(".select2");
+const options_list2 = document.querySelector(".options-list2");
+const options2 = document.querySelectorAll(".option2");
+const faIcon2 = document.querySelector('.arrow2')
+
+//show & hide options list
+select2.addEventListener("click", () => {
+  options_list2.classList.toggle("active2");
+  faIcon2.classList.toggle('active2')
+
+});
+
+//select option
+options2.forEach((option) => {
+  option.addEventListener("click", () => {
+    options2.forEach((option) => {option.classList.remove('selected2')})
+    select2.querySelector("span").innerHTML = option.innerHTML
+    option.classList.add("selected2")
+    options_list2.classList.toggle("active2")
+    faIcon2.classList.toggle('active2')
+
+  })
+})
---
+++ js/script2.js
@@ -0,0 +1,4 @@
+function reprocessAnimation() {
+    const reprocessIcon = document.querySelector('.icon')
+    reprocessIcon.classList.toggle('turn')
+}
